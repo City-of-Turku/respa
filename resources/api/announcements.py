6656b9e1d80d028b07b0be73eb0e6e94850e73ec
ezkat <50319957+ezkat@users.noreply.github.com>
2022-03-11 09:56:34 +0200

    Add maintenance message model

---
+++ resources/api/announcements.py
@@ -0,0 +1,25 @@
+from rest_framework import serializers, viewsets
+from rest_framework.permissions import DjangoModelPermissionsOrAnonReadOnly
+from .base import TranslatedModelSerializer, register_view
+from resources.models import MaintenanceMessage
+
+
+
+class MaintenanceMessageSerializer(TranslatedModelSerializer):
+    class Meta:
+        model = MaintenanceMessage
+        fields = ('message', )
+
+
+
+class MaintenanceMessageViewSet(viewsets.ModelViewSet):
+    queryset = MaintenanceMessage.objects.all()
+    serializer_class = MaintenanceMessageSerializer
+    permission_classes = (DjangoModelPermissionsOrAnonReadOnly, )
+
+
+    def get_queryset(self):
+        return self.queryset.active()
+
+
+register_view(MaintenanceMessageViewSet, 'announcements')
