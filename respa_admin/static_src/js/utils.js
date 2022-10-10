28ae89d43f35cd72f67d38b465a87fe5d739a436
ezkat <50319957+ezkat@users.noreply.github.com>
2022-10-10 12:53:52 +0300

    Paginate reports page resources

--- respa_admin/static_src/js/utils.js
+++ respa_admin/static_src/js/utils.js
@@ -1,6 +1,7 @@
-export function alertPopup(message, type = 'success') {
+export function alertPopup(message, type = 'success', timeout = 5000) {
     let popup = $("div[id=popup-notification]");
     let popupSpan = $(popup).find('span[id=popup-message]');
+    $(popupSpan).addClass('no-select');
 
     switch(type) {
         case 'success':
@@ -13,95 +14,143 @@ export function alertPopup(message, type = 'success') {
         break
     }
 
+    let moveWithScroll = () => {
+        $(popup).css({ 'top': `${($(window).scrollTop())}px` });
+    }
+
+    $(window).on('scroll', moveWithScroll);
+
     $(popupSpan).text(message);
-    $(popup).fadeIn('slow').css('display', 'flex');
+    $(popup).css({
+        'top': `${($(window).scrollTop())}px`,
+        'display': 'flex',
+    }).fadeIn('slow');
 
     setTimeout(() => {
         $(popup).fadeOut('slow');
-        setTimeout(() => {
-        $(popupSpan).text('');
-        }, 500);
-    }, 5000);
+        setTimeout(() => { $(popupSpan).text(''); }, 500);
+        $(window).off('scroll', moveWithScroll);
+    }, timeout);
 }
 
 
 
 export class Paginate {
-    constructor(id, array, perPage, paginationContainer) {
-        this.id = id;
-        this.array = array.reduce((arr, val, i) => {
-            let idx = Math.floor(i / perPage);
-            let page = arr[idx] || (arr[idx] = []);
-            page.push(val);
-            return arr;
-        }, []);
-        this.hide(this.array);
-        this.page = 0;
-        this.paginationContainer = paginationContainer;
-        this.totalPages = this.array.length;
-        this.show(this.current());
+    constructor(main) {
+        this.main = $(main);
+        this.id = this.main.data('paginator-id');
+        this.perPage = this.main.data('paginator-per-page');
+        this.totalPages = 0;
+
+        this.items = this.main
+            .find('[data-paginator-item=true]')
+            .toArray();
+
+        this._pageContainer = this.main
+            .parent()
+            .find(`div[id=paginator-page-container]`)
+            .filter((_, e) => $(e).data('paginator-id') === this.id);
+
+        this.prevPageButton = $(`
+            <a href="javascript://" id=\"prev_page_${this.id}\" class="btn"><span class="glyphicon glyphicon-chevron-left"></span></a>
+            `).appendTo(this._pageContainer)
+            .on('click', (e) => {
+                e.preventDefault();
+                $(this.current()).hide();
+                $(this.previous()).show();
+            });
+
+        this.pageTrackSpan = $(`
+            <span id="paginator-page" class="no-select align-center btn"></span>
+            `).appendTo(this._pageContainer);
+
+        this.nextPageButton = $(`
+            <a href="javascript://" id=\"next_page_${this.id}\" class="btn"><span class="glyphicon glyphicon-chevron-right"></span></a>
+            `).appendTo(this._pageContainer)
+            .on('click', (e) => {
+                e.preventDefault();
+                $(this.current()).hide();
+                $(this.next()).show();
+            });
+        
+        this.prevPageButton.hide();
+        this.nextPageButton.hide();
+
+
+        this.reset();
     }
 
     update() {
-        if (this.totalPages > 1) {
-            if ($(this.paginationContainer).find(`a[id=next_page_${this.id}]`).length === 0) {
-                $(`<a href=\"#\" id=\"next_page_${this.id}\">\>\></a>`)
-                .appendTo(this.paginationContainer)
-                .on('click', (e) => {
-                    e.preventDefault();
-                    $(this.current()).hide();
-                    $(this.next()).show();
-                });
-            }
-        }
-        if (!this.hasNextPage()) $(this.paginationContainer).find(`a[id=next_page_${this.id}]`).remove();
+        if (this.totalPages > 1) $(this.nextPageButton).show();
+        if (!this.hasNextPage()) $(this.nextPageButton).hide();
         if (this.page > 0) {
-            if ($(this.paginationContainer).find(`a[id=prev_page_${this.id}]`).length === 0) {
-                $(`<a href=\"#\" id=\"prev_page_${this.id}\">\<\<</a>`)
-                .prependTo(this.paginationContainer)
-                .on('click', (e) => {
-                    e.preventDefault();
-                    $(this.current()).hide();
-                    $(this.previous()).show();
-                });
-            }
-        } else $(this.paginationContainer).find(`a[id=prev_page_${this.id}]`).remove();
+            $(this.prevPageButton).show();
+        } else $(this.prevPageButton).hide();
 
-        $(this.paginationContainer).find('span').text(`${
+        this.setPageText(`${
             {'fi': 'Sivu', 'en': 'Page', 'sv': 'Page'}[$('html').attr('lang')]
         }: ${this.page + 1}`);
     }
 
+    setPageText(text) { $(this.pageTrackSpan).text(text); }
+
     hasNextPage() {
-        return this.array[this.page + 1] !== undefined && this.array[this.page + 1].length > 0;
+        return this.paginatedItems[this.page + 1] !== undefined && this.paginatedItems[this.page + 1].length > 0;
     }
 
     next() {
         this.page++;
-        this.update();
-        return this.array[this.page];
+        return this.current();
     }
 
     previous() {
         this.page--;
-        this.update();
-        return this.array[this.page];
+        return this.current();
     }
 
     current() {
+        if (!this.paginatedItems[this.page]) {
+            if (this.page >= this.totalPages) {
+                this.page = this.totalPages - 1;
+            } else this.page = 0;
+        }
         this.update();
-        return this.array[this.page];
+        return this.paginatedItems[this.page];
     }
 
-    show(array) {
-        array.forEach((item) => {
-            $(item).show();
-        });
+    show(items = []) { $(items).show(); }
+    hide(items = []) { $(items).hide(); }
+
+    getPaginatedItems(items = []) {
+        return (items.length > 0 ? items : this.items).reduce((arr, val, i) => {
+            let idx = Math.floor(i / this.perPage);
+            let page = arr[idx] || (arr[idx] = []);
+            page.push(val);
+            return arr;
+        }, []);
     }
 
-    hide(array) {
-        array.forEach((item) => {
-            $(item).hide();
-        });
+    reset(page = 0) {
+        page = page < 0 ? 0 : page;
+        this.paginatedItems = this.getPaginatedItems();
+        this.hide(this.items);
+        this.totalPages = this.paginatedItems.length;
+        this.page = page > this.totalPages ? this.totalPages - 1 : page;
+
+        this.show(this.current());
+    }
+
+    filter(string, page = 0) {
+        this.paginatedItems = this.getPaginatedItems(
+            this.items.filter((val) => {
+                let labelString = $(val).find('label').text().toLowerCase();
+                if (labelString.indexOf(string.toLowerCase()) > -1) 
+                    return val;
+            })
+        );
+        this.hide(this.items);
+        this.totalPages = this.paginatedItems.length;
+        this.page = page > this.totalPages ? this.totalPages - 1 : page;
+        this.show(this.current());
     }
 }
