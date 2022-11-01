e49792cd8ea7caaa04ee0c66e5bfbe6cab5fdc65
ezkat <50319957+ezkat@users.noreply.github.com>
2022-11-01 12:19:52 +0200

    use lazy translation

--- qualitytool/models.py
+++ qualitytool/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.conf import settings
-from django.utils.translation import ugettext as _
+from django.utils.translation import ugettext_lazy as _
 from django.utils.functional import lazy
 from solo.models import SingletonModel
 from qualitytool.manager import qt_manager
