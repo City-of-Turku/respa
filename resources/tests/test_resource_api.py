41562450940b39a24da1613ed7d2852f5788984c
ezkat <50319957+ezkat@users.noreply.github.com>
2023-09-05 11:48:23 +0300

    Update test_resource_api.py

--- resources/tests/test_resource_api.py
+++ resources/tests/test_resource_api.py
@@ -1265,6 +1265,7 @@ def test_user_permissions_external_resources(api_client, resource_in_unit, user,
                             is_viewer=False, can_make_reservations=True, can_ignore_opening_hours=False,
                             can_bypass_payment=False, can_create_reservations_for_other_users=False)
 
+
 @pytest.mark.django_db
 def test_user_permissions_in_resource_endpoint_during_maintenance_mode(
     maintenance_mode, resource_in_unit, 
@@ -1273,18 +1274,27 @@ def test_user_permissions_in_resource_endpoint_during_maintenance_mode(
     resource_in_unit.unit.create_authorization(staff_user, 'manager')
     url = reverse('resource-detail', kwargs={'pk': resource_in_unit.pk})
 
+    expected = {
+        'can_bypass_payment': False,
+        'can_ignore_opening_hours': False,
+        'can_make_reservations': False,
+        'is_admin': False,
+        'is_manager': False,
+        'is_viewer': False
+    }
+
 
     api_client.force_authenticate(user=user)
     response = api_client.get(url)
     assert response.status_code == 200
-    assert not any(
-        [permission for _, permission in response.data['user_permissions'].items()]
-    )
+    assert response.data['user_permissions'] == expected
+
+    
+    expected.update({
+        'can_make_reservations_for_customer': False
+    })
 
     staff_api_client.force_authenticate(user=staff_user)
     response = staff_api_client.get(url)
     assert response.status_code == 200
-    assert not any(
-        [permission for _, permission in response.data['user_permissions'].items()]
-    )
-    
+    assert response.data['user_permissions'] == expected
