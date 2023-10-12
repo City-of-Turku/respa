2be3791a1216f175c6cfb9a881bf47c5f9c85860
ezkat <50319957+ezkat@users.noreply.github.com>
2023-10-12 11:54:55 +0300

    Create new endpoint for mass reservation cancels

--- resources/models/reservation.py
+++ resources/models/reservation.py
@@ -88,6 +88,10 @@ class ReservationQuerySet(models.QuerySet):
         allowed_resources = Resource.objects.with_perm('can_view_reservation_catering_orders', user)
         return self.filter(Q(user=user) | Q(resource__in=allowed_resources))
 
+    def cancel(self, user):
+        for reservation in self:
+            reservation.set_state(Reservation.CANCELLED, user)
+
 class ReservationBulkQuerySet(models.QuerySet):
     def current(self):
         return self
