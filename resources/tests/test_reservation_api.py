24804a7ccc1273762a9ba54d57bacc38a01c5fa5
ezkat <50319957+ezkat@users.noreply.github.com>
2024-03-05 10:00:28 +0200

    assert translation

--- resources/tests/test_reservation_api.py
+++ resources/tests/test_reservation_api.py
@@ -25,7 +25,7 @@ from notifications.models import NotificationTemplate, NotificationType
 from notifications.tests.utils import check_received_mail_exists
 from .utils import (
     check_disallowed_methods, assert_non_field_errors_contain,
-    assert_response_objects, MAX_QUERIES
+    assert_response_objects, MAX_QUERIES, assert_translated_response_contains
 )
 
 
@@ -3527,16 +3527,14 @@ def test_recurring_reservation(
 def test_reservation_cooldown(
         resource_with_cooldown, reservation_data,
         api_client, user, list_url):
-    with translation.override('en'):
-        reservation_data['resource'] = resource_with_cooldown.pk
-        api_client.force_authenticate(user=user)
-        response = api_client.post(list_url, data=reservation_data)
-        assert response.status_code == 201, response.json()
+    reservation_data['resource'] = resource_with_cooldown.pk
+    api_client.force_authenticate(user=user)
+    response = api_client.post(list_url, data=reservation_data)
+    assert response.status_code == 201, response.json()
 
-        reservation_data['begin'] = '2115-04-04T12:00:00+02:00'
-        reservation_data['end'] = '2115-04-04T13:00:00+02:00'
-        response = api_client.post(list_url, data=reservation_data)
-        assert response.status_code == 400
-        response_data = response.json()
-        assert response_data['cooldown'][0] == 'Cannot be reserved during cooldown'
+    reservation_data['begin'] = '2115-04-04T12:00:00+02:00'
+    reservation_data['end'] = '2115-04-04T13:00:00+02:00'
+    response = api_client.post(list_url, data=reservation_data)
+    assert response.status_code == 400
+    assert_translated_response_contains(response, 'cooldown', 'Cannot be reserved during cooldown')
 
