7cb462cdd3faa7e389820ab49359b250a2fa0331
Juho Turkki <juho.turkki@gofore.com>
2025-08-27 11:42:38 +0300

    Update deploy-respa.ps1
    Changed personal paths to *path-to-project* text which needs to be user's own path to the project in question

--- deploy-respa.ps1
+++ deploy-respa.ps1
@@ -1,9 +1,9 @@
-Push-Location -Path /Users/terho.craven/Repos/Turku/gofore-respa
+Push-Location -Path *path-to-project*
 ./AzureUtil.ps1 deploy
-./AzureUtil.ps1 dbimport /Users/terho.craven/CopyFiles/Turku/respa/103respa-test.sql
-./AzureUtil.ps1 copyfiles apifiles /Users/terho.craven/CopyFiles/Turku/respa/media
-./AzureUtil.ps1 copyfiles apifiles /Users/terho.craven/CopyFiles/Turku/respa/static
-./AzureUtil.ps1 copyfiles apifiles /Users/terho.craven/CopyFiles/Turku/respa/static_src
-./AzureUtil.ps1 build api /Users/terho.craven/Repos/Turku/gofore-respa
-./AzureUtil.ps1 build ui /Users/terho.craven/Repos/Turku/gofore-varaamo
+./AzureUtil.ps1 dbimport *path-to-project*/103respa-test.sql
+./AzureUtil.ps1 copyfiles apifiles *path-to-project*/media
+./AzureUtil.ps1 copyfiles apifiles *path-to-project*/static
+./AzureUtil.ps1 copyfiles apifiles *path-to-project*/static_src
+./AzureUtil.ps1 build api *path-to-project*
+./AzureUtil.ps1 build ui *path-to-varaamo-project*
 Pop-Location
